c77b969f1bf21a6fa8ccc3d9205d241b23f68a1b
Nattapon Nimakul <totizy@gmail.com>
2019-03-03 09:03:41 +0700

    add missing topbar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div id="nav">
+      <a href="http://krplasma.kulap.io">Wallet</a> |
+      <a href="http://krplasma.kulap.io:8080" class="active">Explorer</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  text-align: center;
+  margin-top: 22px;
+}
+.logo img {
+  height: 80px;
+}
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.active {
+  color: #42b983;
+}
+</style>
