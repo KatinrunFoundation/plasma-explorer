0618b2fb3bfff836b41839b0b238d6f29aba5565
Nattapon Nimakul <totizy@gmail.com>
2019-03-03 08:58:16 +0700

    Init Katinrun with name Katinrun-plasma-mark1 and logo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="app">
+    <TopBar></TopBar>
     <div class="main-container" v-bind:class="{ toggled: sidebarToggled }">
       <div class="side-menu-touch-detector" v-on:click="closeSidebar"></div>
-      <div class="navbar">
+      <div class="navbar" >
+        <img src="https://ipfs.infura.io/ipfs/QmZ1ZFEPt6eQcYZSMX6umB8AzFwQt7kSaptHGMs3QNfk7f" height="20px" style="
+            position: absolute;
+            left: 10px;
+            margin-top: 11opx;
+            padding-top: 10p;
+            height: 47px;
+        ">
         <div class="home-menu">
-          <router-link to="/" class="app-title">Plasma Block Explorer</router-link>
+          <router-link to="/" class="app-title" style="margin-left: 50px;">Katinrun Plasma Block Explorer</router-link>
           <div class="hamburger-icon" v-on:click="toggleSidebar">
             <font-awesome-icon icon="bars" class="hidden-desktop" />
           </div>
@@ -37,15 +45,19 @@
 require('@/assets/css/pure.min.css')
 require('@/assets/css/pure-grid.min.css')
 require('@/assets/css/main.scss')
+import TopBar from '@/components/TopBar.vue'
 
 export default {
   name: 'App',
   metaInfo: {
-    title: 'Plasma Block Explorer',
+    title: 'Katinrun Plasma Block Explorer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ]
+  },
+  components: {
+    TopBar
   },
   data () {
     return {
@@ -65,3 +77,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.active {
+  color: #42b983;
+}
+.navbar {
+  position: fixed;
+  top: 72px;
+}
+</style>
+<style>
+.mobile-view-header {
+  position: fixed;
+  top: 127px;
+}
+</style>
--- src/services/client-service.js
+++ src/services/client-service.js
@@ -1,5 +1,5 @@
 const Plasma = require('plasma-js-lib')
 
-const plasma = new Plasma(new Plasma.providers.HttpProvider({ endpoint: 'https://operator.plasma.group/api' }))
+const plasma = new Plasma(new Plasma.providers.HttpProvider({ endpoint: 'http://139.5.145.39:3000/api' }))
 
 export default plasma
